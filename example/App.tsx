3082a200f7a9326706e13a6aa8ce55c14d60d332
Patrick Kabwe <patrickckabwe@gmail.com>
2025-01-10 09:45:01 +0200

    chore: init android support

--- example/App.tsx
+++ example/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
 import { TextToSpeech } from 'react-native-nitro-tts';
 
 const text =
@@ -7,46 +7,63 @@ const text =
 
 function App(): React.JSX.Element {
   const speak = async () => {
-    try {
-      await TextToSpeech.stop();
-      await TextToSpeech.speak(text, {
+    const opts = Platform.select({
+      ios: {
         pitch: 0.85,
         rate: 0.4,
         volume: 0.9,
         postUtteranceDelay: 0,
         voiceIdentifier: 'com.apple.voice.premium.en-AU.Karen',
-      });
+      },
+      android: {
+        pitch: 1,
+        rate: 1,
+        volume: 2.0,
+        voiceIdentifier: 'en-gb-x-gbd-local',
+      },
+    });
+    try {
+      await TextToSpeech.stop();
+      await TextToSpeech.speak(text, opts);
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    const unsubscribeStart = TextToSpeech.addEventListener('start', () => {
-      console.log('didStart');
-    });
-    const unsubscribeFinish = TextToSpeech.addEventListener('finish', () => {
-      console.log('didFinish');
-    });
-    const unsubscribeWord = TextToSpeech.addEventListener('word', word => {
-      console.log('didWordChange', word);
-    });
+    if (Platform.OS === 'ios') {
+      const unsubscribeStart = TextToSpeech.addListener('start', () => {
+        console.log('didStart');
+      });
+      const unsubscribeFinish = TextToSpeech.addListener('finish', () => {
+        console.log('didFinish');
+      });
+      const unsubscribeWord = TextToSpeech.addListener('word', word => {
+        console.log('didWordChange', word);
+      });
 
-    return () => {
-      unsubscribeFinish();
-      unsubscribeWord();
-      unsubscribeStart();
-    };
+      return () => {
+        unsubscribeFinish();
+        unsubscribeWord();
+        unsubscribeStart();
+      };
+    }
   }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>React Native Text To Speech</Text>
-
-      <Button title="Press to speak" onPress={speak} />
-      <Button title="Press to stop" onPress={TextToSpeech.stop} />
-      <Button title="Press to pause" onPress={TextToSpeech.pause} />
-      <Button title="Press to resume" onPress={TextToSpeech.resume} />
+      <View
+        style={{
+          gap: Platform.select({
+            android: 10,
+          }),
+        }}>
+        <Button title="Press to speak" onPress={speak} />
+        <Button title="Press to stop" onPress={TextToSpeech.stop} />
+        <Button title="Press to pause" onPress={TextToSpeech.pause} />
+        <Button title="Press to resume" onPress={TextToSpeech.resume} />
+      </View>
     </View>
   );
 }
